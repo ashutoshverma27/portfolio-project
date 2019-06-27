218af41e200f75c923758eef028fce97698fe849
Ashutosh Verma <ashutoshverma2799@gmail.com>
2019-06-27 19:06:17 +0530

    Added bootstrap and jobs are showing up

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'jobs.apps.JobsConfig',
+     'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
